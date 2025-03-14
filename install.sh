b31250127cc6fea7d360ad05f8daaa99f7e0143d
Will Shuttleworth <willshuttle21@gmail.com>
2025-03-14 15:12:06 -0400

    switched to gnu stow for symlink management

--- install.sh
+++ install.sh
@@ -1,55 +1,21 @@
 #!/bin/zsh
 
 # install homebrew
-# make symlinks between files in this directory and ones in home directory
-# install homebrew packages
-
-# install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# make symlinks
-files=(".zshrc" ".vimrc" ".gitconfig" ".gitignore_global" "Brewfile" ".tmux.conf")
-dirs=(".vim" "ghostty") 
+brew install stow
 
-cd; echo "moving to home directory"
+dirs=("vim" "ghostty" "git" "brew" "zsh") 
 
-if [ ! -d .old_dotfiles ]; then    
-    mkdir .old_dotfiles
-else
-    rm -r ".old_dotfiles"
-    mkdir .old_dotfiles
-fi
-
-echo "created old_dotfiles dir"
-
-# making directories that need symlinked later
 for dir in ${dirs[@]}; do
     if [ -d $dir ]; then    
-        echo "attempting to move existing dir $dir to ~/.old_dotfiles"
-        mv ~/$dir ~/.old_dotfiles
+        stow $dir
     fi
-    echo "symlinking $dir"
-    ln -s ~/.dotfiles/$dir ~/$dir
+    echo "stowed $dir"
 done
 
-# symlinking all files 
-for file in ${files[@]}; do
-    if [ -f $file ]; then
-        echo "attempting to move existing file $file to ~/.old_dotfiles"
-        mv ~/$file ~/.old_dotfiles
-    fi
-    echo "symlinking $file"
-    ln -s ~/.dotfiles/$file ~/$file
-done    
-
-
 # installing homebrew packages (assuming that Brewfile exists in home directory)
-brew bundle
-
-# tmux plugin manager
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-# install tmux plugins
-~/.tmux/plugins/tpm/bin/install_plugins 
+brew bundle --file brew/Brewfile
 
 # vim plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
